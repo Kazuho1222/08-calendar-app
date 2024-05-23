6d1b6a51f782e464717b1d6b7cbfa9a1d80c025c
Kazuho1222 <destiny5914@gmail.com>
2024-05-24 08:46:54 +0900

    イベント登録実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,164 @@
-import styled from "@emotion/styled";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import FullCalendar from "@fullcalendar/react";
+import '@fullcalendar/common/main.css';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import { styled } from '@mui/system';
+import { useRef, useState } from 'react';
 import './App.css';
+import interactionPlugin from "@fullcalendar/interaction"
 
-function App() {
+// スタイル定義
+const Cover = styled('div')(({
+  opacity: 0,
+  visibility: 'hidden',
+  position: 'fixed',
+  width: '100%',
+  height: '100%',
+  zIndex: 1000,
+  top: 0,
+  left: 0,
+  background: 'rgba(0,0,0,0.3)',
+  '&.inView': {
+    opacity: 1,
+    visibility: 'visible',
+  },
+}));
 
-  const StyleWrapper = styled.div`
+const Form = styled('div')(({
+  opacity: 0,
+  visibility: 'hidden',
+  position: 'fixed',
+  top: '30%',
+  left: '40%',
+  fontWeight: 'bold',
+  background: 'rgba(255,255,255)',
+  width: '400px',
+  height: '300px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  zIndex: 2000,
+  '&.inView': {
+    opacity: 1,
+    visibility: 'visible',
+  },
+}));
+
+const StyleWrapper = styled('div')`
   .fc-day-sat {
     background-color: #eaf4ff;
-    color:red;
+    color: red;
   }
   .fc-day-sun {
-      background-color: #ffeaea;
-      color:red;
+    background-color: #ffeaea;
+    color: red;
   }
-  `
+`;
+
+interface MyEventsType {
+  id: number;
+  title: string;
+  start: string;
+}
+
+function App() {
+  const ref = useRef<FullCalendar>(null);
+  const [inputTitle, setInputTitle] = useState('');
+  const [inView, setInView] = useState(false);
+  const [myEvents, setMyEvents] = useState<MyEventsType[]>([]);
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [newEvent, setNewEvent] = useState<MyEventsType[]>([]);
+
+  const handleClick = (info: any) => {
+    const event = myEvents.find(event => event.id === info.event.id);
+    if (event) {
+      setInputTitle(event.title);
+      setInView(true);
+    }
+  };
+
+  const handleSelect = (selectInfo: any) => {
+    setInputTitle('');
+    setSelectedDate(selectInfo.startStr);
+    setInView(true);
+  };
+
+  const onAddEvent = () => {
+    if (selectedDate) {
+      setNewEvent({
+        id: myEvents.length,
+        title: inputTitle,
+        start: selectedDate,
+      })
+
+    }
+
+    setMyEvents([...myEvents, newEvent]);
+
+    if (ref.current) {
+      ref.current.getApi().addEvent(newEvent);
+    }
+    setInView(false);
+  };
+
+  const coverElement = (
+    <Cover
+      onClick={() => setInView(false)}
+      className={inView ? 'inView' : ''}
+    />
+  );
+
+  const titleElement = (
+    <div>
+      <label>タイトル</label>
+      <input
+        type="text"
+        value={inputTitle}
+        name="inputTitle"
+        onChange={(e) => setInputTitle(e.target.value)}
+      />
+    </div>
+  );
+
+  const btnElement = (
+    <div>
+      <input type="button" value="保存" onClick={onAddEvent} />
+      <input
+        type="button"
+        value="閉じる"
+        onClick={() => setInView(false)}
+      />
+    </div>
+  );
+
+  const formElement = (
+    <Form className={inView ? 'inView' : ''}>
+      <form>
+        <div>予定を入力</div>
+        {titleElement}
+        {btnElement}
+      </form>
+    </Form>
+  );
 
   return (
     <StyleWrapper>
       <div>
-        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth"
-          headerToolbar={{ left: "prev", center: "title", right: "next" }}
-        // businessHours={{ daysOfWeek: [1, 2, 3, 4, 5] }}
+        {coverElement}
+        {formElement}
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={{ left: 'prev', center: 'title', right: 'next' }}
+          ref={ref}
+          eventClick={handleClick}
+          selectable={true}
+          select={handleSelect}
+          events={myEvents}
+          selectAllow={() => true}
         />
       </div>
     </StyleWrapper>
-  )
+  );
 }
 
-export default App
+export default App;
